0977b2f2232dbdc2e58f8ed1fbeef6df89fc2c62
Xopher Dee Pollard <heylisten@xtiv.net>
2014-06-18 23:57:17 -0700

    Update x.engine.php

--- x/x.engine.php
+++ x/x.engine.php
@@ -2,11 +2,11 @@
 	/*
 	 * Point to this file from root & enable Clean URL 
     
-    --- www.domain.com/index.php
-    require('x/x.engine.php');
-
-    --- .htaccess @ www.domain.com/.htaccess
-    <IfModule mod_rewrite.c>
+    	--- www.domain.com/index.php
+    	require('x/x.engine.php');
+	
+	 --- .htaccess @ www.domain.com/.htaccess
+    	<IfModule mod_rewrite.c>
 		RewriteEngine On
 		RewriteCond %{REQUEST_FILENAME} !-f
 		RewriteCond %{REQUEST_FILENAME} !-d
@@ -18,7 +18,7 @@
 	*/
 
 	// Ready
-    // @todo: Keep Being Awesome.
+	// @todo: Keep Being Awesome.
 	require("cfg/cfg.lang.inc");
 	require("cfg/cfg.x.inc");
 	require('lib/x4deep/Xengine.php');
